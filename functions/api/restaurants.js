112a49fafb7e106d1d6d058e20d412819f864ba2
Martin Afani <martin@orderly.ai>
2021-08-09 08:46:41 +0200

    Fetch list of restaurants using firestore api

---
+++ functions/api/restaurants.js
@@ -0,0 +1,27 @@
+const { db } = require("../util/admin");
+
+exports.getAllRestaurants = (request, response) => {
+  db.collection("restaurants")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let restaurants = [];
+      data.forEach((doc) => {
+        restaurants.push({
+          id: doc.id,
+          name: doc.data().name,
+          location: doc.data().location,
+          city: doc.data().city,
+          menu: doc.data().menu,
+          createdAt: doc.data().createdAt
+        });
+      });
+
+      return response.json(restaurants);
+    })
+    .catch((error) => {
+      console.log(error);
+
+      return response.status(500).json({ error: error.code });
+    });
+};
