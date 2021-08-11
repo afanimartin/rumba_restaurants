6be8a1cbe03331a2615931087962f678d159c6d2
Martin Afani <martin@orderly.ai>
2021-08-11 08:50:58 +0200

    Finish sign up logic

--- functions/api/users.js
+++ functions/api/users.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const { admin, db } = require("../util/admin");
 const { firebase } = require("../util/config");
 const { validateLoginData } = require("../util/validators");
@@ -31,5 +30,67 @@ exports.login = (request, response) => {
 };
 
 exports.signup = (request, response) => {
-  
-}
+  const newUser = {
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email,
+    phoneNumber: request.body.phoneNumber,
+    country: request.body.country,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    username: request.body.username,
+  };
+
+  const { valid, errors } = validateSignUpData(newUser);
+
+  if (!valid) return response.status(400).json(errors);
+
+  let token, userId;
+
+  db.collection(`/users/${newUser.username}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return response
+          .status(400)
+          .json({ username: "This username is already taken" });
+      } else {
+        return firebase.auth.createUserWithEmailAndPassword(
+          newUser.email,
+          newUser.password
+        );
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        username: newUser.username,
+        phoneNumber: newUser.phoneNumber,
+        country: newUser.country,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.username}`).set(userCredentials);
+    })
+    .then(() => {
+      return response.status(201).json({ token });
+    })
+    .catch((error) => {
+      console.log(error);
+
+      if (error.code === "auth/email-already-in-use") {
+        return response.status(400).json({ email: "Email already in use" });
+      } else {
+        return response
+          .status(500)
+          .json({ general: "Something went wrong, please try again" });
+      }
+    });
+};
